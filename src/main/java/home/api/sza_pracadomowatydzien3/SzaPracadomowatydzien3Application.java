009f81799fa4ccdf467f67e50d2909bec853a8e3
brzeznicki.lukasz@gmail.com <Gryfek69>
2020-05-02 14:45:23 +0200

    Initial commit

---
+++ src/main/java/home/api/sza_pracadomowatydzien3/SzaPracadomowatydzien3Application.java
@@ -0,0 +1,13 @@
+package home.api.sza_pracadomowatydzien3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzaPracadomowatydzien3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SzaPracadomowatydzien3Application.class, args);
+    }
+
+}
